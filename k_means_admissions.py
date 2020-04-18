d75054ab61b608fbad3d24e3ae9cb665c180213d
Devansh-Maurya <devansh233@gmail.com>
2020-04-18 13:58:20 +0530

    Added some more datasets and code to use admission dataset

--- k_means_admissions.py
+++ k_means_admissions.py
@@ -8,25 +8,26 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('dataset/graduate-admissions/Admission_Predict.csv')
 
 attrs = [i for i in range(1, 7)]
+attrs = [1,6]
 
 x = df.iloc[1:, attrs].values
 x = preprocessing.scale(x)
-
-print(x)
 
 clusters = 4
 
 pca = PCA(n_components=2).fit_transform(x)
 
-kmeans5 = KMeans(n_clusters=clusters)
-y_kmeans5 = kmeans5.fit_predict(pca)
+print(pca)
+
+kmeans = KMeans(n_clusters=clusters)
+y_kmeans5 = kmeans.fit_predict(x)
 
 result = [0 for i in range(clusters)]
 property = {}
 
 for i, y in enumerate(y_kmeans5):
     country = df.iloc[i, 0]
-#    print(country, y)
+    #    print(country, y)
     if y not in property:
         property[y] = []
     property[y].append(country)
@@ -35,9 +36,13 @@ for i, y in enumerate(y_kmeans5):
 for k, v in property.items():
     print(k, ': ', v)
 
+
 #plt.scatter(x[:, 0], x[:, 1], c=y_kmeans5, cmap='rainbow')
 # plt.bar(range(len(result)), result)
-#plt.scatter()
+plt.scatter(df.loc[1:, 'GRE Score'], df.loc[1:, 'TOEFL Score'], c=kmeans.labels_.astype(float))
+centroids = kmeans.cluster_centers_
+
+plt.scatter(centroids[:, 0], centroids[:, 1], marker='X', s=169, color='b')
 plt.show()
 
 #Elbow method
