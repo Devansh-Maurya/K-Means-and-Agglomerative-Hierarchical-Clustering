222d4caeaf5b4774e6bd3bf8308e7babe32f5fde
Devansh-Maurya <devansh233@gmail.com>
2020-04-19 20:15:48 +0530

    Able to show a different widget on clicking KMeans button

--- ui/app.py
+++ ui/app.py
@@ -1,30 +1,21 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtWidgets import QHBoxLayout
-from PyQt5.QtWidgets import QPushButton
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QHBoxLayout
+
+def showKMeansMenu():
+    layout = QHBoxLayout()
+    layout.addWidget(uic.loadUi('uifiles/kmeans.ui'))
+    window.frame.setLayout(layout)
+
 
 app = QtWidgets.QApplication(sys.argv)
 
 window = uic.loadUi("app.ui")
+window.pbKMeans.clicked.connect(showKMeansMenu)
+
+print(window.frame)
+
+
 window.show()
 app.exec()
-
-# app = QApplication(sys.argv)
-#
-# window = QWidget()
-# window.setWindowTitle('Data Mining Assignment')
-#
-# layout = QHBoxLayout()
-# layout.addWidget(QPushButton('K-Means Clustering'))
-# layout.addWidget(QPushButton('Agglomerative hierarchical clustering'))
-#
-# window.setLayout(layout)
-#
-# window.show()
-# sys.exit(app.exec())
-
-
-
