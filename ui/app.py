15cb2fbb6b5bcf08f0d8deae5f48efb52aaa325c
Devansh-Maurya <devansh233@gmail.com>
2020-04-19 21:01:14 +0530

    Stopping GUI development and switching to CLI app

--- ui/app.py
+++ ui/app.py
@@ -3,16 +3,22 @@ import sys
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QHBoxLayout
 
-def showKMeansMenu():
+def showKMeansUi():
+    widget = uic.loadUi('uifiles/kmeans.ui')
     layout = QHBoxLayout()
-    layout.addWidget(uic.loadUi('uifiles/kmeans.ui'))
+    layout.addWidget(widget)
+
+    kValues = [str(i) for i in range(2, 10)]
+
+    widget.comboBox.addItems(kValues)
+
     window.frame.setLayout(layout)
 
 
 app = QtWidgets.QApplication(sys.argv)
 
 window = uic.loadUi("app.ui")
-window.pbKMeans.clicked.connect(showKMeansMenu)
+window.pbKMeans.clicked.connect(showKMeansUi)
 
 print(window.frame)
 
